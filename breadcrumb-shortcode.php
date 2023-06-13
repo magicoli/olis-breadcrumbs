0ed2a9e86e0fa7f1ce9ead86aa41c47dff8c453a
Olivier van Helden <olivier@van-helden.net>
2023-06-13 08:00:13 -0400

    Initial commit

---
+++ breadcrumb-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Breadcrumb Shortcode
+ * Plugin URI:      https://magiiic.com
+ * Description:     Register a [breadcrumb] shortcode for inclusion in posts or templates.
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com
+ * Text Domain:     breadcrumb-shortcode
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Breadcrumb_Shortcode
+ */
+
+// Your code starts here.
