2d99c222fe2c56c23f797fa8e2482e9ba1902bd5
Olivier van Helden <olivier@van-helden.net>
2023-06-13 08:06:30 -0400

    * initial shortcode

--- breadcrumb-shortcode.php
+++ breadcrumb-shortcode.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Breadcrumb Shortcode
  * Plugin URI:      https://magiiic.com
- * Description:     Register a [breadcrumb] shortcode for inclusion in posts or templates.
+ * Description:     Register [breadcrumb] shortcode for inclusion in posts or templates.
  * Author:          Magiiic
  * Author URI:      https://magiiic.com
  * Text Domain:     breadcrumb-shortcode
@@ -12,4 +12,42 @@
  * @package         Breadcrumb_Shortcode
  */
 
-// Your code starts here.
+// Code starts here.
+class Breadcrumb {
+    public function init() {
+        add_shortcode('breadcrumb', array($this, 'breadcrumb_shortcode'));
+    }
+
+    public function breadcrumb_shortcode($atts) {
+        // Get the current post
+        $post = get_post();
+
+        // Check if the post exists
+        if ($post) {
+            $breadcrumbs = '<ul class="breadcrumb">';
+
+            // Add the home link
+            $breadcrumbs .= '<li><a href="' . esc_url(home_url('/')) . '">' . esc_html__('Home', 'text-domain') . '</a></li>';
+
+            // Get the post type
+            $post_type = get_post_type($post);
+
+            // Add the post type archive link if it's available
+            if (get_post_type_archive_link($post_type)) {
+                $breadcrumbs .= '<li><a href="' . esc_url(get_post_type_archive_link($post_type)) . '">' . esc_html__(ucfirst($post_type), 'text-domain') . '</a></li>';
+            }
+
+            // Add the current post title
+            $breadcrumbs .= '<li>' . esc_html(get_the_title($post)) . '</li>';
+
+            $breadcrumbs .= '</ul>';
+
+            return $breadcrumbs;
+        }
+
+        return ''; // If no post is found, return empty string
+    }
+}
+
+$breadcrumb = new Breadcrumb();
+add_action('init', array($breadcrumb, 'init'));
