a0d3162926a43964fe23bafd4a3810359fd5f200
Olivier van Helden <olivier@van-helden.net>
2023-06-13 11:58:48 -0400

    * added exclude-home, exclude-title and separator parameters to shortcode and divi module

--- breadcrumb-divi.php
+++ breadcrumb-divi.php
@@ -10,11 +10,13 @@ function register_breadcrumb_divi_module() {
   if (class_exists('ET_Builder_Module')) {
     class Breadcrumb_Divi_Module extends ET_Builder_Module {
       public $slug = 'et_pb_custom_breadcrumb';
-      
+
       public function init() {
         $this->name = esc_html__('Custom Breadcrumb', 'et_builder');
         $this->whitelisted_fields = array(
+          'exclude_home',
           'exclude_title',
+          'separator',
         );
         $this->advanced_setting_title_text = esc_html__('New Breadcrumb', 'et_builder');
         $this->settings_text = esc_html__('Breadcrumb Settings', 'et_builder');
@@ -23,6 +25,17 @@ function register_breadcrumb_divi_module() {
 
       public function get_fields() {
         return array(
+          'exclude_home' => array(
+            'label' => esc_html__('Exclude Home', 'et_builder'),
+            'type' => 'yes_no_button',
+            'options' => array(
+              'on' => esc_html__('Yes', 'et_builder'),
+              'off' => esc_html__('No', 'et_builder'),
+            ),
+            'option_category' => 'configuration',
+            'description' => esc_html__('Exclude the home link from the breadcrumb.', 'et_builder'),
+            'default' => 'off',
+          ),
           'exclude_title' => array(
             'label' => esc_html__('Exclude Title', 'et_builder'),
             'type' => 'yes_no_button',
@@ -34,14 +47,23 @@ function register_breadcrumb_divi_module() {
             'description' => esc_html__('Exclude the post title from the breadcrumb.', 'et_builder'),
             'default' => 'off',
           ),
+          'separator' => array(
+            'label' => esc_html__('Separator', 'et_builder'),
+            'type' => 'text',
+            'option_category' => 'configuration',
+            'description' => esc_html__('Specify the separator character for the breadcrumb.', 'et_builder'),
+            'default' => '/',
+          ),
         );
       }
 
       public function shortcode_callback($atts, $content = null, $function_name) {
+        $exclude_home = $this->shortcode_atts['exclude_home'] === 'on';
         $exclude_title = $this->shortcode_atts['exclude_title'] === 'on';
+        $separator = $this->shortcode_atts['separator'];
 
-        // Process the [breadcrumb] shortcode with the exclude-title option
-        $breadcrumb = do_shortcode('[breadcrumb exclude-title="' . ($exclude_title ? 'true' : 'false') . '"]');
+        // Process the [breadcrumb] shortcode with the specified options
+        $breadcrumb = do_shortcode("[breadcrumb exclude-home=\"" . ($exclude_home ? 'true' : 'false') . "\" exclude-title=\"" . ($exclude_title ? 'true' : 'false') . "\" separator=\"" . esc_attr($separator) . "\"]");
         return $breadcrumb;
       }
     }
@@ -66,16 +88,16 @@ add_action('admin_init', 'integrate_breadcrumb_divi_module');
 // Manually add the Custom Breadcrumb module to Divi Builder
 function add_breadcrumb_divi_module($modules) {
   $modules['et_pb_custom_breadcrumb'] = array(
-  'name' => 'Custom Breadcrumb',
-  'slug' => 'et_pb_custom_breadcrumb',
-  'type' => 'shortcode',
-  'child_title_var' => 'title',
-  'child_title_fallback_var' => 'title',
-  'child_title_value' => esc_html__('Breadcrumb', 'et_builder'),
-  'advanced_setting_title_text' => esc_html__('New Breadcrumb', 'et_builder'),
-  'settings_text' => esc_html__('Breadcrumb Settings', 'et_builder'),
-  'settings_text' => esc_html__('Breadcrumb Settings', 'et_builder'),
-  'render_callback' => 'et_pb_render_breadcrumb_divi_module',
+    'name' => 'Custom Breadcrumb',
+    'slug' => 'et_pb_custom_breadcrumb',
+    'type' => 'shortcode',
+    'child_title_var' => 'title',
+    'child_title_fallback_var' => 'title',
+    'child_title_value' => esc_html__('Breadcrumb', 'et_builder'),
+    'advanced_setting_title_text' => esc_html__('New Breadcrumb', 'et_builder'),
+    'settings_text' => esc_html__('Breadcrumb Settings', 'et_builder'),
+    'settings_text' => esc_html__('Breadcrumb Settings', 'et_builder'),
+    'render_callback' => 'et_pb_render_breadcrumb_divi_module',
   );
 
   return $modules;
@@ -85,5 +107,5 @@ add_filter('et_builder_modules', 'add_breadcrumb_divi_module');
 // Render callback for the Custom Breadcrumb module
 function et_pb_render_breadcrumb_divi_module($attrs, $content = null, $render_slug) {
   $module = new Breadcrumb_Divi_Module();
-  return $module->shortcode_callback($attrs, $content, $render_slug);
+  return "(preview) " . $module->shortcode_callback($attrs, $content, $render_slug);
 }
--- breadcrumb-shortcode.php
+++ breadcrumb-shortcode.php
@@ -14,57 +14,89 @@
 
 // Code starts here.
 class Breadcrumb {
-  public function init() {
-    ( !defined('BREADCRUMB_VERSION' ) && define( 'BREADCRUMB_VERSION', '0.1' ) );
-    add_shortcode('breadcrumb', array($this, 'breadcrumb_shortcode'));
-  }
+    private $counter = 0;
 
-  function breadcrumb_shortcode($atts) {
-    $atts = shortcode_atts(array(
-        'exclude-title' => 'false'
-    ), $atts);
-
-    // Get the current post
-    $post = get_post();
-
-    // Check if the post exists
-    if ($post) {
-      $breadcrumbs = '<ul class="breadcrumb" style="list-style: none; display: inline;">';
-
-      // Add the home link
-      $breadcrumbs .= '<li style="display: inline;"><a href="' . esc_url(home_url('/')) . '">' . esc_html__( 'Home' ) . '</a></li>';
-
-      // Get the post categories
-      $categories = get_the_category($post->ID);
-
-      // Add the post category links if available
-      if ($categories) {
-        foreach ($categories as $category) {
-          $breadcrumb_link = get_category_link($category->term_id);
-          $breadcrumb_title = $category->name;
-
-          // Check if the category has parent categories
-          if ($category->parent) {
-            $breadcrumb_title = get_category_parents($category->term_id, true, '</li><li>', false) . $breadcrumb_title;
-          }
-
-          $breadcrumbs .= '<li class="breadcrumb" style="display: inline;"><a href="' . esc_url($breadcrumb_link) . '">' . esc_html($breadcrumb_title) . '</a></li>';
-        }
-      }
-
-      // Add the current post title if exclude-title is true
-      if ($atts['exclude-title'] !== 'true') {
-          $breadcrumbs .= '<li class="breadcrumb title" style="display: inline;">' . esc_html(get_the_title($post)) . '</li>';
-      }
-
-      $breadcrumbs .= '</ul>';
-
-      return $breadcrumbs;
+    public function init() {
+        ( !defined('BREADCRUMB_VERSION' ) && define( 'BREADCRUMB_VERSION', '0.1' ) );
+        add_shortcode('breadcrumb', array($this, 'breadcrumb_shortcode'));
     }
 
-    return ''; // If no post is found, return empty string
-  }
+    public function breadcrumb_shortcode($atts) {
+        $atts = shortcode_atts(array(
+            'exclude-home' => 'false',
+            'exclude-title' => 'false',
+            'separator' => '/',
+        ), $atts);
 
+        // Get the current post
+        $post = get_post();
+
+        // Check if the post exists
+        if ($post) {
+          $this->counter++;
+
+            $breadcrumbs = '<ul class="breadcrumbs breadcrumbs-' . $this->counter . '" style="list-style: none; display: inline;">';
+
+            // Add the home link
+            if ($atts['exclude-home'] !== 'true') {
+              $breadcrumbs .= '<li class="breadcrumb-home" style="display: inline;"><a href="' . esc_url(home_url('/')) . '">' . esc_html__( 'Home' ) . '</a></li>';
+            }
+
+            // Get the post categories
+            $categories = get_the_category($post->ID);
+
+            // Add the post category links if available
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $breadcrumb_link = get_category_link($category->term_id);
+                    $breadcrumb_title = $category->name;
+
+                    // Check if the category has parent categories
+                    if ($category->parent) {
+                        $breadcrumb_title = get_category_parents($category->term_id, true, '</li><li>', false) . $breadcrumb_title;
+                    }
+
+                    $breadcrumbs .= '<li class="breadcrumb-parent" style="display: inline;"><a href="' . esc_url($breadcrumb_link) . '">' . esc_html($breadcrumb_title) . '</a></li>';
+                }
+            }
+
+            // Add the current post title if exclude-title is true
+            if ($atts['exclude-title'] !== 'true') {
+                $breadcrumbs .= '<li class="breadcrumb-title" style="display: inline;">' . esc_html(get_the_title($post)) . '</li>';
+            }
+
+            $breadcrumbs .= '</ul>';
+
+            // Generate dynamic CSS for separator
+            $separator_css = sprintf(
+              '
+              #left-area ul.breadcrumbs-%1$s,
+              .entry-content ul.breadcrumbs-%1$s,
+              .et-l--body ul.breadcrumbs-%1$s,
+              .et-l--footer ul.breadcrumbs-%1$s,
+              .et-l--header ul.breadcrumbs-%1$s {
+                padding-left: 0;
+                padding-right: 0;
+              }
+              .breadcrumbs-%1$s li:not(:first-child):before {
+                content: "%2$s";
+                margin: 0 5px;
+              }',
+              $this->counter,
+              esc_attr($atts['separator']),
+            );
+
+            // Register and enqueue breadcrumb-style stylesheet
+            wp_register_style('breadcrumb-style-' . $this->counter, false);
+            wp_enqueue_style('breadcrumb-style-' . $this->counter);
+
+            wp_add_inline_style('breadcrumb-style-' . $this->counter, $separator_css);
+
+            return $breadcrumbs;
+        }
+
+        return ''; // If no post is found, return empty string
+    }
 }
 
 $breadcrumb = new Breadcrumb();
